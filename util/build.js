c0cc9321211be1bb30ef50e9d78b5bdb85651b6e
Ubunbun <kentoglwo@gmail.com>
2019-12-12 20:06:13 +0900

    :wrench: modify util files

--- util/build.js
+++ util/build.js
@@ -8,7 +8,19 @@ try {
   fs.copySync("./src/public", "./dist/public");
   fs.copySync("./src/views", "./dist/views");
   // Transpile the typescript files
-  childProcess.exec("tsc --build tsconfig.prod.json");
+  childProcess.exec(
+    "tsc --build tsconfig.prod.json",
+    (error, stdout, stderr) => {
+      if (error) {
+        console.error(`[ERROR] ${error}`);
+        return;
+      }
+      fs.renameSync("./dist/src/Server.js", "./dist/Server.js");
+      fs.renameSync("./dist/src/start.js", "./dist/start.js");
+      fs.renameSync("./dist/src/routes", "./dist/routes");
+      fs.removeSync("./dist/src");
+    }
+  );
 } catch (err) {
   console.log(err);
 }
