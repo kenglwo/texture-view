f08015658d65f926804ba7b2d02eb359384befff
Ubunbun <kentoglwo@gmail.com>
2019-12-27 15:42:24 +0900

    :sparkles: add new router for sizzle and thanks words

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -3,6 +3,8 @@ import * as ReactDOM from "react-dom";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import IndexPage from "./index/IndexPage";
 import ResultPage from "./result//ResultPage";
+import ResultPageSizzle from "./result_sizzle/ResultPageSizzle";
+import ResultPageThanks from "./result_thanks/ResultPageThanks";
 import "../public/stylesheets/custom.scss";
 
 interface Props {}
@@ -16,6 +18,14 @@ export default class Router extends React.Component<Props, State> {
         <Route
           path="/result/:keyword/:texture/:offset"
           component={ResultPage}
+        />
+        <Route
+          path="/result_sizzle/:keyword/:sizzle_word/:offset"
+          component={ResultPageSizzle}
+        />
+        <Route
+          path="/result_thanks/:keyword/:thanks_word/:offset"
+          component={ResultPageThanks}
         />
       </BrowserRouter>
     );
