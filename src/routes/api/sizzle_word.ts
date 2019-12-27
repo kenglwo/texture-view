2c8a239e442875e899bda082033c4c298c387d37
Ubunbun <kentoglwo@gmail.com>
2019-12-27 17:22:43 +0900

    :sparkles: update api to apply categorical color of sizzle words

--- src/routes/api/sizzle_word.ts
+++ src/routes/api/sizzle_word.ts
@@ -33,6 +33,7 @@ pool.on("error", (err, client) => {
 router.get("/", (req: Request, res: Response, next: NextFunction) => {
   const query = url.parse(req.url, true).query;
   const recipe_id = query.recipe_id;
+  console.log(recipe_id);
 
   (async () => {
     const client = await pool.connect();
@@ -41,22 +42,25 @@ router.get("/", (req: Request, res: Response, next: NextFunction) => {
     const result = await client.query(
       `
         select
-          sizzle,
-          count(*) AS count
+          t1.sizzle,
+          count(*) AS count,
+          t2.category_big_id
         from
-          recipe_sizzle
+          recipe_sizzle as t1
+          inner join sizzle_305_category as t2 using(sizzle)
         where recipe_id = $1
-        group by sizzle
+        group by 1, 3
         order by count desc;
-
-        ;
     `,
       [recipe_id]
     );
 
     if (result.rowCount === 0) {
       console.log("------> 該当するレシピはありませんでした．");
-      res.redirect(302, `${process.env.HOST}:${process.env.PORT}/noresult`);
+      res.redirect(
+        302,
+        `http://${process.env.HOST}:${process.env.PORT}/noresult`
+      );
     } else {
       client.release();
 
@@ -64,11 +68,11 @@ router.get("/", (req: Request, res: Response, next: NextFunction) => {
       result.rows.map(item => {
         const obj: WordCloudCategorySizzleElement = {
           text: String(item.sizzle),
-          value: Number(item.count)
+          value: Number(item.count),
+          category_big_id: Number(item.category_big_id)
         };
         json.push(obj);
       });
-      console.log(json);
 
       res.header("Access-Control-Allow-Origin", "*");
       res.header(
