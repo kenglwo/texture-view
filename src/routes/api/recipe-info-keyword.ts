50c9fc2f3ed10eee65ea4f54f4dc7e5bd6726286
Ubunbun <kentoglwo@gmail.com>
2020-01-12 19:46:25 +0900

    :sparkles: update api to change recipe_order

--- src/routes/api/recipe-info-keyword.ts
+++ src/routes/api/recipe-info-keyword.ts
@@ -20,22 +20,40 @@ import { RecipeItemKeyword } from "../../components/models/Types";
 
 router.get("/", (req: Request, res: Response, next: NextFunction) => {
   const query = url.parse(req.url, true).query;
+  console.log(query);
   const keyword = query.keyword;
-  const offset = 0;
+  const recipe_order = query.order;
+  // console.log(`recipe_order: ${recipe_order}`);
 
-  const RECIPE_INFO: RecipeItemKeyword[] = [];
+  let recipe_order_query = "";
 
-  (async () => {
-    const client = await pool.connect();
+  switch (recipe_order) {
+    case "new":
+      recipe_order_query = "t1.published_at asc";
+      break;
+    case "review":
+      recipe_order_query = "t2.review_count desc";
+      break;
+    case "texture":
+      recipe_order_query = "t2.texture_count desc";
+      break;
+    case "sizzle":
+      recipe_order_query = "t2.sizzle_count desc";
+      break;
+    case "thanks":
+      recipe_order_query = "t2.thanks_count desc";
+      break;
+    default:
+      recipe_order_query = "t1.published_at asc";
+  }
 
-    try {
-      const result = await client.query(
-        `
+  const sql = `
         select
           t1.recipe_id
           , substr(t1.user_id, 1,5) as user_id
           , t1.title
           , t1.description
+          , t1.published_at
           , t2.review_count
           , t2.texture_count
           , t2.sizzle_count
@@ -47,10 +65,23 @@ router.get("/", (req: Request, res: Response, next: NextFunction) => {
         where
           t1.title like $1
         group by
-          1,2,3,4,5,6,7,8
-        limit 10 offset $2 ;`,
-        [`%${keyword}%`, offset]
-      );
+          1,2,3,4,5,6,7,8, 9
+        order by ${recipe_order_query}
+        limit 10
+        offset $2
+    `;
+
+  const offset = 0;
+  const params = [`%${keyword}%`, offset];
+
+  const RECIPE_INFO: RecipeItemKeyword[] = [];
+
+  (async () => {
+    const client = await pool.connect();
+
+    try {
+      console.log(`try: recipe_order_query: ${recipe_order_query}`);
+      const result = await client.query(sql, params);
 
       if (result.rowCount !== 0) {
         console.log(`------> 検索結果: ${result.rows[0].recipe_count} 件`);
@@ -59,6 +90,8 @@ router.get("/", (req: Request, res: Response, next: NextFunction) => {
         // res.redirect(302, "http://kento/ex-gen-app/noresult");
         res.redirect(302, `${process.env.HOST}:${process.env.PORT}/noresult`);
       }
+
+      console.log(result.rows[0]);
 
       _.forEach(result.rows, elem => {
         const RECIPE_JSON: RecipeItemKeyword = {
